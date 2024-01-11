9afde8df2d26cc92bff9d038cc11c25c97cd5887
Suraj Singh <surajsingh04012002@gmail.com>
2024-01-11 16:09:17 +0530

    chore(cors): origin change

--- Backend/server.js
+++ Backend/server.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 // cors
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://voosh-frontend-bpn4.onrender.com",
     credentials: true,
   })
 );
