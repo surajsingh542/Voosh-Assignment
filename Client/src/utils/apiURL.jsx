20e136bdfb8355e39c5dea7a35f7285c3eb00dfa
Suraj Singh <surajsingh04012002@gmail.com>
2024-01-11 15:55:05 +0530

    chore (apiURL): Changed to deployed url

--- Client/src/utils/apiURL.jsx
+++ Client/src/utils/apiURL.jsx
@@ -1,4 +1,4 @@
-// let URL = "https://stormy-tiara-slug.cyclic.app/api";
-let URL = "http://localhost:4000";
+// let URL = "https://voosh-backend-vrfz.onrender.com";
+let URL = "https://voosh-backend-vrfz.onrender.com";
 
 export const API_URL_BASE = URL;
